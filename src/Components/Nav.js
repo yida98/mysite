fdb27f1443e9dce4437e9db8622057f2fa22ad34
Yida Da <carriedada@gmail.com>
2020-10-27 00:02:01 -0700

    fixed nav with some warnings

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,45 +1,42 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
 import './Nav.css'
 
 function Nav() {
 
     const [navItems, setItems] = useState([
-        // {
-        //   id: 2,
-        //   title: "projects",
-        //   active: true,
-        //   link: '/',
-        // },
         {
           id: 1,
           title: "projects",
-          active: true,
+          active: window.location.pathname == "/",
           link: '/',
         },
         {
           id: 0,
           title: "about me",
-          active: false,
+          active: window.location.pathname == "/about",
           link: '/about',
         },
       ]);
-    const [activeId, setActive] = useState(1)
+
+    const [activeItem, setActive] = useState(navItems.filter(item => item.link == window.location.pathname)[0])
     
     const click = useCallback((item) => (event) => {
         const newItems = [...navItems]
         newItems.map((i) => {
-            return(i.id === item.id ? ((i.active = true) && (setActive(i.id))) : i.active = false);
+            return(i.id === item.id ? ((i.active = true) && (setActive(i))) : i.active = false);
         })
         setItems(newItems)
+
     }, [navItems]);
-    
+
+
     return (
         <div id="navbar">
             <div id="highlight" style={{
-                right: `${(activeId) * 140}px`,
+                right: `${(activeItem.id) * 140}px`,
             }}></div>
-            <h1 className={`${activeId === 0 ? 'about' : ''}`}>YIDA ZHANG</h1>
+            <h1 className={`${activeItem.id === 0 ? 'about' : ''}`}>YIDA ZHANG</h1>
                 <div id="buttons">
                 {navItems.map((item, index) => {
                     return(
